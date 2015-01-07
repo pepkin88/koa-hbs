f5cc3cca3abd99df9e37349722ade6249987851e
Marek Pepke <pepkin88@gmail.com>
2015-01-07 23:34:25 +0100

    Revert "Fixing race conditions with clearing blocks"
    This reverts commit 689ffc1f2b9dfdce1a09639e3acdcbde9dcde6ac.

--- index.js
+++ index.js
@@ -161,6 +161,9 @@ Hbs.prototype.createRenderer = function() {
     // conditionally clear cache
     if (hbs.disableTemplateCaching) hbs.cache = {};
 
+    // clear blocks
+    hbs.blocks = {};
+
     // Initialization... move these actions into another function to remove
     // unnecessary checks
     if((!hbs.partialsRegistered || hbs.disableTemplateCaching) && hbs.partialsPath !== '')
@@ -172,21 +175,14 @@ Hbs.prototype.createRenderer = function() {
     // Load the template
     if(!hbs.cache[tpl]) {
       rawTemplate = yield read(tplPath);
-      var isLayoutSpecified = rLayoutPattern.test(rawTemplate);
-      // Load layout if specified
-      if (isLayoutSpecified) {
-        var layout = rLayoutPattern.exec(rawTemplate)[1];
-        var rawLayout = yield hbs.loadLayoutFile(layout);
-      }
-
-      // all yields before clearing blocks
-      hbs.blocks = {};
-
       hbs.cache[tpl] = {
         template: hbs.handlebars.compile(rawTemplate)
       };
 
-      if (isLayoutSpecified) {
+      // Load layout if specified
+      if(rLayoutPattern.test(rawTemplate)) {
+        var layout = rLayoutPattern.exec(rawTemplate)[1];
+        var rawLayout = yield hbs.loadLayoutFile(layout);
         if (!hbs.cache[tpl]) hbs.cache[tpl] = {
           template: hbs.handlebars.compile(rawTemplate)
         };
